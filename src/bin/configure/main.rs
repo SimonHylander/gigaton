3fa4fc8496bd3b0b8be45110fef86cbd34f69bf5
Simon Hylander <hylandersimon@gmail.com>
2023-02-02 12:52:13 +0100

    config file

--- src/bin/configure/main.rs
+++ src/bin/configure/main.rs
@@ -1,20 +1,29 @@
-use std::{env::{self, home_dir}, fs::create_dir, path::Path};
+use std::{
+    env,
+    fs::{create_dir, File},
+    path::Path,
+};
 fn main() {
     println!("{}", env::consts::OS);
 
-
-    let h = home_dir();
-
-    let dir = "~/.gigaton";
-    let path = Path::new(dir);
-    let config_file = ".gigaton/gigaton.yml";
+    let config = dirs::config_dir().unwrap();
+    let path = Path::new(config.as_path());
 
     if !path.is_dir() {
-        println!("{} does not exist.", dir);
-        create_dir(path).expect("Could not create directory");
+        create_dir(path).expect(
+            "Could not c
+        reate directory",
+        );
     }
 
-    println!("{}", config_file);
+    let config_file = Path::new(path).join("gigatom.yaml");
+
+    if !config_file.exists() {
+        let mut file = File::open(config_file)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        assert_eq!(contents, "Hello, world!");
+    }
 
     if env::consts::OS == "linux" {}
 }
